dabe7d164d5eaaa20b0f6b35d1a82827b8ba2e65
Shonal Machado <shonal.machado@lifelock.com>
2017-04-20 16:39:38 -0700

    VPC template changes

--- vpc/vpc_template.tf
+++ vpc/vpc_template.tf
@@ -19,24 +19,138 @@ resource "aws_internet_gateway" "default" {
 }
 
 
-# Grant the VPC internet access on its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-
-# Create a subnet to launch our instances into
+# Public Subnet
 resource "aws_subnet" "public" {
   vpc_id                  = "${aws_vpc.default.id}"
   cidr_block              = "${var.public_subnet_cidr}"
   availability_zone       = "${var.public_subnet_avail_zone}"
   map_public_ip_on_launch = true
+  tags {
+    Name = "terraform-public-subnet"
+  }
 }
 
-# For future use
-#resource "aws_subnet" "s_private" {
-#    vpc_id = "${aws_vpc.s_vpc.id}"
-#    cidr_block = "10.1.2.0/24"
+resource "aws_route_table" "public_route_table" {
+  vpc_id         = "${aws_vpc.default.id}"
+  route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+  tags {
+        Name = "Public-Subnet-route-table"
+    }
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+    subnet_id = "${aws_subnet.public.id}"
+    route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+# Private Subnet
+resource "aws_subnet" "private" {
+    vpc_id = "${aws_vpc.default.id}"
+    cidr_block = "${var.private_subnet_cidr}"
+    availability_zone = "${var.private_subnet_avail_zone}"
+    tags {
+        Name = "terraform-private-subnet"
+    }
+}
+
+resource "aws_route_table" "private_route_table" {
+    vpc_id = "${aws_vpc.default.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        instance_id = "${aws_instance.nat.id}"
+    }
+    tags {
+        Name = "Private-Subnet-route-table"
+    }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+    subnet_id = "${aws_subnet.private.id}"
+    route_table_id = "${aws_route_table.private_route_table.id}"
+}
+
+
+/*
+  NAT Instance
+*/
+resource "aws_security_group" "nat_sg" {
+    name = "vpc_nat"
+    description = "Allow traffic to pass from the private subnet to the internet"
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+    egress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "terraform-NATSG"
+    }
+}
+
+resource "aws_instance" "nat-instance" {
+    ami = "ami-30913f47" # this is a special ami preconfigured to do NAT
+    availability_zone = "${var.public_subnet_avail_zone}"
+    instance_type = "${var.nat_instance_type}"
+    key_name = "${var.aws_key_name}"
+    vpc_security_group_ids = ["${aws_security_group.nat_sg.id}"]
+    subnet_id = "${aws_subnet.public.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+    tags {
+        Name = "terraform-VPC-NAT"
+    }
+    user_data = "${file("nat-userdata.sh")}"
+}
+
+#resource "aws_eip" "nat" {
+#    instance = "${aws_instance.nat.id}"
+#    vpc = true
 #}
